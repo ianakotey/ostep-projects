a80f3dfdbae4ef54173c6db9fd016797a19a0a52
Ian Akotey <ianakotey@gmail.com>
2022-10-25 09:02:11 +0000

    refactored to reveal missing file

--- concurrency-bank/tests/correctness/test-bank-correct.py
+++ concurrency-bank/tests/correctness/test-bank-correct.py
@@ -1,25 +1,34 @@
+#! /usr/bin/python3
+
 import argparse
 from pathlib import Path
 import transactionutils
 
 import logging
+
 logging.getLogger().setLevel(logging.INFO)
 
 parser = argparse.ArgumentParser()
 
+
+parser.add_argument("binary", type=Path)
 parser.add_argument("balance", type=int)
 parser.add_argument("husband", type=Path)
 parser.add_argument("wife", type=Path)
 
 args = parser.parse_args()
 
-if any(
-    map(lambda file: not file.exists(), (args.husband, args.wife))
-):
-    print("Error from test: All files must exist!")
-    exit(1)
 
-if not (binary := Path("../../bank")).exists():
+files: tuple[Path] = tuple(
+    map(lambda file: file.absolute(), (args.binary, args.husband, args.wife))
+)
+
+for file in files:
+    if not file.exists():
+        print(f"File: {file} does not exist")
+        exit(1)
+
+if not (binary := Path(args.binary).absolute()).exists():
     print("bank executable does not exist")
     exit(1)
 
