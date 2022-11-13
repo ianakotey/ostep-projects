80e365c22132d11c5d9c9df820ffc83893b826ba
Ian Akotey <ianakotey@gmail.com>
2022-11-13 02:23:27 +0000

    marked as executable, added filename parameter

--- concurrency-bank/tests/correctness/generate_file.py
+++ concurrency-bank/tests/correctness/generate_file.py
@@ -1,30 +1,33 @@
+#! /usr/bin/python3
 
-
+from typing import Optional
 import transactionutils
 import argparse
 from pathlib import Path
+import shutil
+
 
 parser = argparse.ArgumentParser()
 
-
-parser.add_argument("n_lines", type=int, default=1000)
-parser.add_argument("amount_range", default="-50:3000")
-parser.add_argument("perc_empty", type=float, default=0.00)
-parser.add_argument("perc_withdraw", type=float, default=0.50)
-parser.add_argument("output_path", type=Path, default=Path.cwd())
+parser.add_argument( "-n", "--n_lines", type=int, default=1000, required=False )
+parser.add_argument( "-r", "--amount_range", default="-50:3000", required=False )
+parser.add_argument( "-pe", "--perc_empty", type=float, default=0.00, required=False )
+parser.add_argument( "-pw", "--perc_withdraw", type=float, default=0.50, required=False )
+parser.add_argument( "-o", "--output_path", type=Path, default=Path.cwd(), required=False )
+parser.add_argument( "-f", "--file_name", type=str, default=None, required=False )
 args = parser.parse_args()
 
 f_args = {
     "n_lines": args.n_lines,
-    "amount_range": range( *args.amount_range.split( ":" ) ),
+    "amount_range": range( *list( map( lambda x: int(x), args.amount_range.split( ":" ) ) ) ),
     "perc_empty": args.perc_empty,
     "perc_withdraw": args.perc_withdraw,
     "perc_withdraw": args.perc_withdraw,
 }
 
-
 file = transactionutils.TransactionTester.create_test_file( **f_args )
 
-file.rename( args.output_path / file.name )
+out_file = args.output_path / (args.file_name if args.file_name else file.name)
+shutil.move( file, out_file )
 
-print(file)
+print( out_file )
