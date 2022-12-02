01a2eaea52b640595bd79812e34ef21b804a85ab
Ian Akotey <ianakotey@gmail.com>
2022-12-02 20:13:06 +0000

    added option for concurrency testing

--- concurrency-bank/tests/correctness/test-bank-correct.py
+++ concurrency-bank/tests/correctness/test-bank-correct.py
@@ -2,6 +2,7 @@
 
 import argparse
 from pathlib import Path
+from typing import Sequence
 import transactionutils
 
 import logging
@@ -15,6 +16,7 @@ parser.add_argument("binary", type=Path)
 parser.add_argument("balance", type=int)
 parser.add_argument("husband", type=Path)
 parser.add_argument("wife", type=Path)
+parser.add_argument("-c", "--custom", action="store_true")
 
 args = parser.parse_args()
 
@@ -22,6 +24,27 @@ args = parser.parse_args()
 files: tuple[Path] = tuple(
     map(lambda file: file.absolute(), (args.binary, args.husband, args.wife))
 )
+
+
+def verify_concurrency_nop(output: list[transactionutils.TransactionRecord]) -> bool:
+    logging.debug("Using null concurrency test")
+    return True
+
+
+def verify_concurrency(output: list[transactionutils.TransactionRecord]) -> bool:
+    """
+    Assert that a series of transactions were interleaved
+    """
+    logging.debug(f"running concurrency test on {len(output)} records")
+    quart = output[:(len(output)>>2)]
+    return sorted(quart, key=lambda x: x.user) != quart
+
+
+if args.custom:
+    logging.info(f"Using multithreaded analyzer")
+    validator = verify_concurrency
+else:
+    validator = verify_concurrency_nop
 
 for file in files:
     if not file.exists():
@@ -33,7 +56,8 @@ if not (binary := Path(args.binary).absolute()).exists():
     exit(1)
 
 if transactionutils.TransactionTester(binary).run_and_test_output(
-    args.balance, args.husband.absolute(), args.wife.absolute()
+    args.balance, args.husband.absolute(), args.wife.absolute(),
+    validator
 ):
     print("Passed")
     exit(0)
