e78f5ff2e15758d29f8bd48ed73c463221237572
Ian Akotey <ianakotey@gmail.com>
2022-12-03 00:10:16 +0000

    bugfix

--- concurrency-bank/tests/grader/grade.sh
+++ concurrency-bank/tests/grader/grade.sh
@@ -18,11 +18,11 @@ if [[ -z $3 ]]; then
     mkdir -p "$src_dir"/out
     out_dir="$src_dir"/out
 else
-    test_dir="$(realpath -s "$3")"
+    out_dir="$(realpath -s "$3")"
 fi
 
-global_log="$out_dir"/all.log
-global_scores="$out_dir"/all.csv
+global_log="$out_dir/all.log"
+global_scores="$out_dir/all.csv"
 
 csv_header="name, Test 1, Test 2, Test 3, Test 4, Test 5, Test 6, Test 7, Test 8, Test 9, Test 10, Test 11, Test 12, Test 13, Test 14, Test 15, Test 16, Test 17, Test 18, Test 19"
 
